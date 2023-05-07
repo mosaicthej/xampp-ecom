c502d431065f6bd3340eaa004ff031b8fc554532
Mark Jia <mjia@ualberta.ca>
2023-05-07 05:51:50 -0600

    Add recursive base case using default parent id=-1.
    Note that, this assume that the table `category` has entry *-1*.
    
    In case of a new spawned table `category`, consider adding such new category:
    
    ```sql
    INSERT INTO `category` (`id`, `category_name`, `category_desc`, `parent_category`, `deleted`) VALUES ('-1', 'top', NULL, '-1', '0');
    ```

--- site/category/process_add_category.php
+++ site/category/process_add_category.php
@@ -4,6 +4,7 @@ require_once '../includes/functions.php';
 
 $category_name = $_POST['category_name'];
 $category_desc = $_POST['category_desc'];
+$parentCategoryId = isset($_POST['parentCategoryId']) ? $_POST['parentCategoryId'] : -1; // Change the default value to -1
 
 $stmt = dbExecute("INSERT INTO category (category_name, category_desc, parent_category) VALUES (:category_name, :category_desc, :parentCategoryId)", [
     ':category_name' => $category_name,
@@ -13,11 +14,9 @@ $stmt = dbExecute("INSERT INTO category (category_name, category_desc, parent_ca
 
 $newCategoryId = $conn->lastInsertId();
 
-if ($parentCategoryId !== null) { // Add this condition
-    dbExecute("INSERT INTO category_subcategories (category_id, child_id) VALUES (:categoryId, :childId)", [
-        ':categoryId' => $parentCategoryId,
-        ':childId' => $newCategoryId
-    ]);
-}
+dbExecute("INSERT INTO category_subcategories (category_id, child_id) VALUES (:categoryId, :childId)", [
+    ':categoryId' => $parentCategoryId,
+    ':childId' => $newCategoryId
+]);
 
 require_once 'process_fetch_categories.php';
--- site/category/process_fetch_categories.php
+++ site/category/process_fetch_categories.php
@@ -2,16 +2,21 @@
 require_once '../includes/db_connection.php';
 require_once '../includes/functions.php';
 
-function fetchCategories($parentId = null) {
+function fetchCategories($parentId = -1) {
     $categories = [];
 
-    if ($parentId === null) {
-        $stmt = dbExecute("SELECT * FROM category WHERE parent_category IS NULL AND deleted = 0")['stmt'];
+    if ($parentId == -1) {
+        $stmt = dbExecute("SELECT * FROM category WHERE parent_category = -1 AND deleted = 0")['stmt'];
     } else {
         $stmt = dbExecute("SELECT c.* FROM category c JOIN category_subcategories cs ON c.id = cs.child_id WHERE cs.category_id = :parentId AND c.deleted = 0 AND cs.deleted = 0", [':parentId' => $parentId])['stmt'];
     }
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // Add this condition to prevent fetching the same top-level category repeatedly
+        if ($row['id'] == $parentId) {
+            continue;
+        }
+
         $category = [
             'id' => $row['id'],
             'name' => $row['category_name'],
