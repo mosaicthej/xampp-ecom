795050cf13fbab4c8c0515c353d723f0898995ad
Mark Jia <mjia@ualberta.ca>
2023-05-08 09:24:41 -0600

    hotfix

--- site/includes/fetch_regions.php
+++ site/includes/fetch_regions.php
@@ -4,6 +4,7 @@ require_once './functions.php';
 
 $country_id = $_GET['country_id'];
 
+$query = "SELECT * FROM regions WHERE country_id = ?";
 $regions = dbExecute($query, [$country_id])['stmt']->fetchAll(PDO::FETCH_ASSOC);
 
 header('Content-Type: application/json');
